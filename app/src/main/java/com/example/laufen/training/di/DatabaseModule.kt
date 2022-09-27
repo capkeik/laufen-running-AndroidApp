ceaa09de8595b599eb451e75b74700270517c247
CapKeik <azrkhamzin@gmail.com>
2022-09-27 15:50:13 +0300

    add app class && database module

---
+++ app/src/main/java/com/example/laufen/training/di/DatabaseModule.kt
@@ -0,0 +1,29 @@
+package com.example.laufen.training.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.laufen.training.data.data_source.room.TrainingDao
+import com.example.laufen.training.data.data_source.room.TrainingDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DatabaseModule {
+    @Provides
+    fun provideDatabase(trainingDatabase: TrainingDatabase): TrainingDao {
+        return trainingDatabase.TrainingDao
+    }
+
+    @Provides
+    fun provideTrainingDatabase(
+        @ApplicationContext appContext: Context
+    ) = Room.databaseBuilder(
+        appContext,
+        TrainingDatabase::class.java,
+        TrainingDatabase.name
+    ).build()
+}
