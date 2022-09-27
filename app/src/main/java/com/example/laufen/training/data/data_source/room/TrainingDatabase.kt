c841c8ae246de07508da1717c37abdf34ae73db9
CapKeik <azrkhamzin@gmail.com>
2022-09-27 15:48:45 +0300

    add database parent

--- app/src/main/java/com/example/laufen/training/data/data_source/room/TrainingDatabase.kt
+++ app/src/main/java/com/example/laufen/training/data/data_source/room/TrainingDatabase.kt
@@ -1,13 +1,14 @@
 package com.example.laufen.training.data.data_source.room
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.laufen.training.domain.entity.Training
 
 @Database(
     entities = [Training::class],
     version = 1
 )
-abstract class TrainingDatabase {
+abstract class TrainingDatabase: RoomDatabase() {
     abstract val TrainingDao: TrainingDao
 
     companion object {
