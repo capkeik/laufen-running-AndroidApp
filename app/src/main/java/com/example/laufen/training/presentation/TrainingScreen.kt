8fb47f3163949f6f741b3bbe92bb98281bd6795e
CapKeik <azrkhamzin@gmail.com>
2022-09-28 21:58:45 +0300

    add googleMaps view

--- app/src/main/java/com/example/laufen/training/presentation/TrainingScreen.kt
+++ app/src/main/java/com/example/laufen/training/presentation/TrainingScreen.kt
@@ -2,23 +2,22 @@ package com.example.laufen.training.presentation
 
 import android.Manifest
 import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.ScaffoldState
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.laufen.R
+import com.example.laufen.training.Const.ACTION_START_OR_RESUME
 import com.example.laufen.training.permission.PermissionState
+import com.example.laufen.training.service.Polyline
 import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.JointType
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.*
 
@@ -27,17 +26,11 @@ private const val TAG = "training_screen"
 fun TrainingScreen() {
     val viewModel: TrainingViewModel = viewModel()
     val context = LocalContext.current
-    val currentPosition = viewModel.currentPosition.collectAsState()
-    val trainingState = viewModel.trainingState.collectAsState()
-    val cameraPositionState = CameraPositionState(
-        position = CameraPosition.fromLatLngZoom(currentPosition.value, 10f)
-    )
-
-    LaunchedEffect(key1 = Unit) {
-        if (!viewModel.isProviderInitialised) {
-            viewModel.initLocation(context)
-        }
-    }
+    val distance = viewModel.curDistance.collectAsState()
+    val curPosition = viewModel.curPosition.collectAsState()
+    val updateCounter = viewModel.updateCounter.collectAsState()
+    val polylines = viewModel.polylines.collectAsState()
+    val isGranted = viewModel.isPermissinoGranted.collectAsState()
 
     PermissionUI(
         context,
@@ -48,6 +41,7 @@ fun TrainingScreen() {
         when (permissionAction) {
             is PermissionState.OnPermissionGranted -> {
                 Log.d(TAG, "Permission grant successful")
+                viewModel.grant()
             }
             is PermissionState.OnPermissionDenied -> {
                 Log.d(TAG, "Permission grant denied")
@@ -55,17 +49,88 @@ fun TrainingScreen() {
         }
     }
 
+    if (isGranted.value) {
+        LaunchedEffect(Unit) {
+            if (!viewModel.isServiceInited && isGranted.value) {
+                viewModel.sendServiceCommand(
+                    action = ACTION_START_OR_RESUME,
+                    context
+                )
+            }
+        }
+
+        val cameraPositionState =
+            CameraPositionState(CameraPosition.fromLatLngZoom(curPosition.value, 15f))
+
+        val mapProperties by remember { mutableStateOf(MapProperties(isMyLocationEnabled = true)) }
+        val uiSettings by remember { mutableStateOf(MapUiSettings(myLocationButtonEnabled = true)) }
+
+
+        Column() {
+            Text(
+                text = "${curPosition.value} / ${updateCounter.value}"
+            )
+            GoogleMap(
+                modifier = Modifier.fillMaxSize(),
+                cameraPositionState = cameraPositionState,
+                uiSettings = uiSettings,
+                properties = mapProperties,
+            ) {
+                for (p in listOf<List<LatLng>>(
+                    listOf(
+                        LatLng(55.79328781735486, 49.12508607707867),
+                        LatLng(55.793782400477795, 49.12514508567111),
+                        LatLng(55.79434935389224, 49.12528992494345),
+                        LatLng(55.794479028247544, 49.125300653778446),
+                        LatLng(55.794587592491986, 49.125150450088604)
+                    ),
+                    listOf(
+                        LatLng(55.79361351914194, 49.1241204819297),
+                        LatLng(55.79423777328747, 49.123160251198236),
+                        LatLng(55.79383969933607, 49.12167967196982)
+                    )
+                )) {
+                    Polyline(
+                        points = p,
+                        jointType = JointType.ROUND
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Map(
+    modifier: Modifier,
+    cameraPositionState: CameraPositionState,
+    uiSettings: MapUiSettings,
+    mapProperties: MapProperties
+) {
     GoogleMap(
         modifier = Modifier.fillMaxSize(),
-        cameraPositionState
+        cameraPositionState = cameraPositionState,
+        uiSettings = uiSettings,
+        properties = mapProperties,
     ) {
-        Marker(
-            state = MarkerState(position = currentPosition.value!!),
-            title = "Singapore"
-        )
-    }
-
-    Column() {
-        Text(currentPosition.value.toString())
+        for (p in listOf<List<LatLng>>(
+            listOf(
+                LatLng(55.79328781735486, 49.12508607707867),
+                LatLng(55.793782400477795, 49.12514508567111),
+                LatLng(55.79434935389224, 49.12528992494345),
+                LatLng(55.794479028247544, 49.125300653778446),
+                LatLng(55.794587592491986, 49.125150450088604)
+            ),
+            listOf(
+                LatLng(55.79361351914194, 49.1241204819297),
+                LatLng(55.79423777328747, 49.123160251198236),
+                LatLng(55.79383969933607, 49.12167967196982)
+            )
+        )) {
+            Polyline(
+                points = p,
+                jointType = JointType.ROUND
+            )
+        }
     }
 }
--- app/src/main/java/com/example/laufen/training/presentation/TrainingViewModel.kt
+++ app/src/main/java/com/example/laufen/training/presentation/TrainingViewModel.kt
@@ -1,46 +1,121 @@
 package com.example.laufen.training.presentation
 
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import com.example.laufen.training.utils.LocationProvider
+import com.example.laufen.training.service.Polyline
+import com.example.laufen.training.service.Polylines
+import com.example.laufen.training.service.TrainingService
 import com.google.android.gms.maps.model.LatLng
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-data class TrainingState(
-    val duration: Long = 0,
-    val distance: Long = 0,
-    val burnedCalories: Int = 0,
-    val route: List<LatLng> = listOf(),
-)
 
 class TrainingViewModel: ViewModel() {
-    private val _trainingState = MutableStateFlow(TrainingState())
-    val trainingState = _trainingState.asStateFlow()
-    private val _latLng = MutableStateFlow( LatLng(1.35, 103.87))
-    private val _route = MutableStateFlow<List<LatLng>>(listOf())
-    val currentPosition = _latLng.asStateFlow()
-    var isProviderInitialised = false
-    private lateinit var locationProvider: LocationProvider
-    private val currentLocationObserver = Observer<LatLng>() {
-        updatePosition(it)
+    val isServiceInited = false
+    val _isPermissionGranted = MutableStateFlow(false)
+    private var _isTracking = false
+    private var _pathPoints = MutableStateFlow(mutableListOf<Polyline>())
+    private var _curTimeInMillis = 0L
+
+    val isPermissinoGranted = _isPermissionGranted.asStateFlow()
+
+    fun grant() {
+        _isPermissionGranted.value = true
     }
 
-    fun initLocation(context: Context) {
-        locationProvider = LocationProvider(context)
-        isProviderInitialised = true
-        locationProvider.liveLocation.observeForever(currentLocationObserver)
-        locationProvider.getUserLocation()
+
+    private val trackingObserver = Observer<Boolean> { _isTracking = it }
+    private val millisObserver = Observer<Long> { _curTimeInMillis = it }
+    private val pathObserver = Observer<Polylines> { updatePathPoints(it) }
+
+    private val _curAvgSpeed = MutableStateFlow(0)
+    private val _curDistance = MutableStateFlow(0L)
+    private val _curDuration = MutableStateFlow(0L)
+    private val _curCalories = MutableStateFlow(0L)
+    private val _curPosition = MutableStateFlow(LatLng(1.35, 103.87))
+
+    val curAvgSpeed = _curAvgSpeed.asStateFlow()
+    val curDistance = _curDistance.asStateFlow()
+    val curDuration = _curDuration.asStateFlow()
+    val curCalories = _curCalories.asStateFlow()
+    val curPosition = _curPosition.asStateFlow()
+    val polylines = _pathPoints.asStateFlow()
+
+    private val _updateCounter = MutableStateFlow(0)
+    val updateCounter = _updateCounter.asStateFlow()
+
+    private fun setAvgSpeed(value: Int) {
+        _curAvgSpeed.value = value
     }
 
-    private fun updatePosition(latLng: LatLng) {
-        _route.value += latLng
-        _latLng.value = latLng
+    private fun setDuration(value: Long) {
+        _curDuration.value = value
+    }
+
+    private fun setDistance(value: Long) {
+        _curDistance.value = value
+    }
+
+    private fun setCalories(value: Long) {
+        _curCalories.value = value
+    }
+
+    private fun updatePathPoints(polylines: Polylines) {
+        _pathPoints.value = polylines
+        Log.d("list", polylines.toString())
+        _updateCounter.value += 1
+        if (_pathPoints.value.isNotEmpty() && _pathPoints.value.last().isNotEmpty()) {
+            _curPosition.value = _pathPoints.value.last().last()
+        }
+    }
+
+    fun sendServiceCommand(action: String, context: Context) {
+        Intent(context, TrainingService::class.java).also {
+            it.action = action
+            context.startService(it)
+        }
+    }
+
+    init {
+        TrainingService.isTracking.observeForever(trackingObserver)
+//        TrainingService.curTimeInMillis.observeForever(millisObserver)
+        TrainingService.pathPoints.observeForever(pathObserver)
     }
 
     override fun onCleared() {
-        locationProvider.liveLocation.removeObserver(currentLocationObserver)
+        with(TrainingService) {
+            isTracking.removeObserver(trackingObserver)
+//            curTimeInMillis.removeObserver(millisObserver)
+            pathPoints.removeObserver(pathObserver)
+        }
         super.onCleared()
     }
+
+
+//    private val _trainingState = MutableStateFlow(TrainingState())
+//    val trainingState = _trainingState.asStateFlow()
+//    private val _latLng = MutableStateFlow( LatLng(1.35, 103.87))
+//    private val _route = MutableStateFlow<List<LatLng>>(listOf())
+//    val currentPosition = _latLng.asStateFlow()
+//    var isProviderInitialised = false
+//    private lateinit var locationProvider: LocationProvider
+//    private val currentLocationObserver = Observer<LatLng>() {
+//        updatePosition(it)
+//    }
+//
+//    fun initLocation(context: Context) {
+//        locationProvider = LocationProvider(context)
+//        isProviderInitialised = true
+//        locationProvider.liveLocation.observeForever(currentLocationObserver)
+//        locationProvider.getUserLocation()
+//    }
+//
+//    private fun updatePosition(latLng: LatLng) {
+//        _route.value += latLng
+//        _latLng.value = latLng
+//    }
+//
 }
